16efab4e5a313fa314e5426d314a07b50ad7c69d
subtanjiroo <138212507+subtanjiroo@users.noreply.github.com>
2024-07-16 11:51:06 +0700

    Add files via upload

---
+++ backend/app.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const cinemaRoute = require("./routes/cinema");
+const cilentRoute = require("./routes/cilents");
+const roomRoute = require("./routes/room");
+// const orderRoute = require("./routes/order");
+const LCRoute = require("./routes/logCilent");
+const LORoute = require("./routes/logOrder");
+const AUU = require("./routes/AU");
+const db = require("./config/Database");
+const app = express();
+const bodyParser = require('body-parser');
+// Define a route
+app.use(cors({
+  origin: 'https://minh-3gsb-3gptlqni2-subtanjiroos-projects.vercel.app/' // Đổi thành domain thật của frontend
+}));
+dotenv.config();
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
+app.use("/movie_order", cinemaRoute);
+app.use("/cilentIF", cilentRoute);
+app.use("/Room", roomRoute);
+// app.use("/Order", orderRoute);
+app.use("/logCilent", LCRoute);
+app.use("/logOrder", LORoute);
+app.use("/AUUser", AUU);
+app.get('/test', (req, res) => {
+  res.json({ message: 'API is working' });
+});
+const port = process.env.PORT;
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
